a722b193f98a9de1e5840aadc4e91ac470f71b19
root <75710541@qq.com>
2016-10-24 20:18:24 +0800

    es add auto clean

--- check.sh
+++ check.sh
@@ -10,7 +10,3 @@ check_port logstash 5011
 check_port kibana 5601
 curl http://127.0.0.1:9200/_cluster/health 2>/dev/null|python -m json.tool|grep 'status.*green' &>/dev/null && echo "elasticsearch is ok" || (echo "elasticsearch is error"; exit 1)
 
-
-
-
-
--- run.sh
+++ run.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -e
+
 base_dir=$(cd `dirname $0` && pwd)
 cd $base_dir
 ./build_config.sh
 
+job_cmd="/data/offlinesry/offline-elk/clean_es.sh"
+
+crontab -l 2>/dev/null|grep -F "$job_cmd" || (crontab -l 2>/dev/null;echo "0 4 * * * $job_cmd" ) | crontab -
+
 docker-compose -p elk up -d
+
