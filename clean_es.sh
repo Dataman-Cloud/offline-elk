77fc98f91ec9e2a4db0db6a600bf46cd69c38f1d
ss75710541 <75710541@qq.com>
2016-10-27 11:06:09 +0800

    update clean_es

--- clean_es.sh
+++ clean_es.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-
+base_dir=$(cd `dirname $0` && pwd)
+cd $base_dir
 . ../config.cfg
 
 docker exec -it elk_elasticsearch_1 curator --timeout 36000 --host 127.0.0.1 delete indices --older-than $ES_DATA_KEEP_DAY --time-unit days --timestring '%Y-%m-%d' --prefix "dataman-"
